e4f568b2a81bb3433ba3b474e5890b3762c62f98
Christopher Wells <cwellsny@nycap.rr.com>
2017-09-22 20:20:47 -0400

    Set to pickle results

--- lcimages/__main__.py
+++ lcimages/__main__.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os.path
 import pandas as pd
+import pickle
 import sys
 
 import images
@@ -12,12 +13,13 @@ def main():
     data_file = sys.argv[1]
     source_curves_dir = sys.argv[2]
     target_curves_dir = sys.argv[3]
+    bins_file = sys.argv[4]
 
     data = pd.read_csv(data_file)
 
     ids = data[ID]
 
-    for i in ids:
+    def process_curve(i):
         curve_file = source_curves_dir + "/" + str(i) + ".csv"
 
         if os.path.isfile(curve_file):
@@ -31,6 +33,13 @@ def main():
 
             img = images.bins_to_image(bins)
             img.save(target_curves_dir + "/" + str(i) + ".png", "PNG")
+
+            return bins
+
+    bin_sets = [process_curve(i) for i in ids]
+
+    with open(bins_file, "wb") as f:
+        pickle.dump(bin_sets, f)
 
 if __name__ == "__main__":
     main()
